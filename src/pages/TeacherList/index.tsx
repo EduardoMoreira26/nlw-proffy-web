e74d0cf853c8cedff015dcc32fe1aaa34de89bc4
Eduardo <du_du.mba@hotmail.com>
2021-01-03 20:56:42 +0000

    create routes and header's studypage

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,50 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg';
-
-import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
 
-const Landing = () => {
+const TeacherList = () => {
 
   return (
-    <div id="page-landing">
-      <div id="pade-landing-content" className="container">
-        <div className="logo-container">
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
           <img src={logoImg} alt="Proffy" />
-          <h2>Sua plataforma de estudos online</h2>
         </div>
 
-        <img
-          src={landingImg}
-          alt="Plataforma de estudos"
-          className="hero-image"
-        />
-
-        <div className="buttons-container">
-          <a href="" className="study">
-            <img src={studyIcon} alt="Estudar" />
-            Estudar
-          </a>
-
-          <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aulas" />
-            Dar aulas
-          </a>
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis.</strong>
         </div>
 
-        <span className="total-connections">
-          Total de 200 conexões
-          <img src={purpleHeartIcon} alt="Coração roxo" />
-        </span>
-
-      </div>
+      </header>
     </div>
   );
 }
 
-export default Landing;
+export default TeacherList;
